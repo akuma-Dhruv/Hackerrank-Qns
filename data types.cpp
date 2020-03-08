6db276d5f0478f5b6e4b710dc7f99c66680b2bee
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-03-08 11:23:27 +0530

    data types

---
+++ data types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long int i;
+    float f;
+     double d;
+     char c;
+    unsigned long l;
+    cin>>i;
+    cin>>l;
+    cin>>c;
+    cin>>f;
+    cin>>d;
+    cout<<i<<endl<<l<<endl<<c<<endl;
+    
+    cout<<fixed<<f<<endl;
+ 
+    cout<<fixed<<d;
+    return 0;
+}
