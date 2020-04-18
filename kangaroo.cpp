62ff500e94d0f4b47db2cd5dadefe5184056618c
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-04-18 22:14:18 +0530

    runtime error cleared
    runtime error cleared for test case 7
    (equal value of velocity)

--- kangaroo.cpp
+++ kangaroo.cpp
@@ -18,17 +18,23 @@ string kangaroo(int x1, int v1, int x2, int v2) {
     {
         return n;
     }
-    else if((x2-x1)%(v1-v2)==0)
+    else if (x1!=x2&&v1==v2) {
+        return n;
+    }
+    else if((x2-x1)%(v1-v2)==0&&v1!=v2)
     {
         return y;
     }
+    
      if(x1==x2&&(v2>v1||v1>v2))
 {
     return n;
 }
-if((x1>x2||x2>x1)&&v1==v2){return n;}
+if((x1>x2||x2>x1)&&v1==v2)
+{return n;}
 return n;
 }
+
 
 int main()
 {
