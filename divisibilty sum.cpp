fef10a064e2c8cb9b564c8ca2d44a27b41ea54af
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-04-30 00:06:33 +0530

    Divisibility sum hackerrank

--- divisibilty sum.cpp
+++ divisibilty sum.cpp
@@ -1,1 +1,20 @@
-
+//https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+int divisibleSumPairs(int n, int k, vector<int> array) 
+{cout<<n;
+    int i=0,j=1,count=0;
+    
+    
+ for(i = 0; i < n ; i++)
+{
+for(j = 0; j < n ; j++)
+{
+if(i < j)
+{
+if((array[i] + array[j]) % k == 0)
+count++;
+}
+}
+}
+cout<<count;
+return count;
+}
